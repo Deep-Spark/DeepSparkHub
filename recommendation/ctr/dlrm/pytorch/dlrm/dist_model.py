ee027e37600cd62a400667d50d4606823d1b1b61
sanghui_ilu <11972788+sanghui-ilu@user.noreply.gitee.com>
2022-11-11 09:31:20 +0000

    !12 dlrm model add checkpoint * dlrm model add checkpoint

--- recommendation/ctr/dlrm/pytorch/dlrm/dist_model.py
+++ recommendation/ctr/dlrm/pytorch/dlrm/dist_model.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2022, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
 """Distributed version of DLRM model
 
 In order to code the hybrid decomposition, the model code needs to be restructured. I don't know a clean enough
@@ -427,3 +443,10 @@ class DistDlrm():
     def to(self, *args, **kwargs):
         self.bottom_model.to(*args, **kwargs)
         self.top_model.to(*args, **kwargs)
+
+    def state_dict(self):
+        dlrm_state_dic = {}
+        dlrm_state_dic.update(self.bottom_model.state_dict())
+        dlrm_state_dic.update(self.top_model.state_dict())
+
+        return dlrm_state_dic
