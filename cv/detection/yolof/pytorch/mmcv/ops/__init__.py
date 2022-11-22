bbca8af11a3fbb20a4cba00f03b092c04d9c582a
wenfeng.zhang <wenfeng.zhang@iluvatar.ai>
2022-11-22 14:59:38 +0800

    add license for yolof

--- cv/detection/yolof/pytorch/mmcv/ops/__init__.py
+++ cv/detection/yolof/pytorch/mmcv/ops/__init__.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2022, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
 # Copyright (c) OpenMMLab. All rights reserved.
 from .info import (get_compiler_version, get_compiling_cuda_version)
 from .nms import nms, batched_nms
