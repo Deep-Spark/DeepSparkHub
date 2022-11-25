5129a5c3a938df12d2e051bb515d77dbe2e30be3
jianyong.jiang <jianyong.jiang@iluvatar.local>
2022-11-25 11:03:10 +0800

    Update license in bert, retinanet, segmentation.

--- cv/semantic_segmentation/ocrnet/pytorch/init.sh
+++ cv/semantic_segmentation/ocrnet/pytorch/init.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Iluvatar CoreX. All rights reserved.
+# Copyright (c) 2022, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
 rm -rf log/
 rm -rf output/
 pip3 install -r requirements.txt
