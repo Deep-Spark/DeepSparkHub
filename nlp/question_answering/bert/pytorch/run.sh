775d0d24ed1f7a8d594caca68a4c2ba833237120
牛冠博 <guanbo.niu@iluvatar.ai>
2023-02-17 23:43:44 +0800

    add bert squad code

---
+++ nlp/question_answering/bert/pytorch/run.sh
@@ -0,0 +1,11 @@
+python3 run_qa.py \
+  --model_name_or_path "/home/data/bert/bert-base-uncased-pt" \
+  --dataset_name squad \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size 12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir /tmp/debug_squad/
