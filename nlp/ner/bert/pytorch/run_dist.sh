4af68a8a07516d48659997892046e680df73f6db
牛冠博 <guanbo.niu@iluvatar.com>
2023-03-09 16:18:40 +0800

    change License

--- nlp/ner/bert/pytorch/run_dist.sh
+++ nlp/ner/bert/pytorch/run_dist.sh
@@ -1,16 +1,17 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 python3 -m torch.distributed.launch --nproc_per_node=8 --master_port 12333  \
   run_ner.py \
