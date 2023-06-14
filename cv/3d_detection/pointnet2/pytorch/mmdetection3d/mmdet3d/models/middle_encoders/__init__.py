592381852a7d8d60c4d16ea588c3a2c312382a19
张文风 <wenfeng.zhang@iluvatar.ai>
2023-06-14 09:52:19 +0000

    !161 add pointnet2 pytorch model * add pointnet2 pytorch model

---
+++ cv/3d_detection/pointnet2/pytorch/mmdetection3d/mmdet3d/models/middle_encoders/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+# Copyright (c) OpenMMLab. All rights reserved.
+from .pillar_scatter import PointPillarsScatter
+# from .sparse_encoder import SparseEncoder, SparseEncoderSASSD
+# from .sparse_unet import SparseUNet
+
+__all__ = [
+    'PointPillarsScatter'
+]
+
+# __all__ = [
+#     'PointPillarsScatter', 'SparseEncoder', 'SparseEncoderSASSD', 'SparseUNet'
+# ]
