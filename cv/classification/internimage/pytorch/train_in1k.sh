9ab2766f46ccd18397fee3e94b5a8d8b5757c267
majorli <mingjiang.li@iluvatar.ai>
2023-07-10 05:59:45 +0000

    !178 add internimage pytorch model base on 3.1.0 * add requirements * update copyright * add mmcv build and install * add internimage pytorch model

---
+++ cv/classification/internimage/pytorch/train_in1k.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+
+
+set -x
+
+PARTITION=$1
+JOB_NAME=$2
+CONFIG=$3
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-12}
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+    srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \
+    --quotatype=reserved \
+    ${SRUN_ARGS} \
+    python3 -u main.py \
+    --cfg ${CONFIG} \
+    --accumulation-steps 1 \
+    --local_rank 0 \
+    --data-path /home/datasets/cv/imagenet \
+    --output work_dirs ${@:4}
