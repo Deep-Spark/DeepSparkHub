561975411359488ab72c959f44da6c8eeced2d24
吴永乐 <yongle.wu@iluvatar.ai>
2023-08-17 02:06:48 +0000

    !198 add FaceNet TensorFlow model * add FaceNet TensorFlow model

---
+++ cv/face/facenet/tensorflow/src/align/run.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+for N in {1..150}; do
+  python align_dataset_mtcnn.py /iluvatar/webface/CASIA-WebFace /iluvatar/webface/webface_160 --image_size 182 --margin 32 --random_order --gpu_memory_fraction 0.9 &
+done
