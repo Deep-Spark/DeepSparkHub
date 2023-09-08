0c3cdf311749947a53315907725595b76f3b5af0
牛斯克 <guanbo.niu@iluvatar.com>
2023-09-08 10:40:14 +0000

    !210 Add ColossalAI LLaMA 7B model * Add ColossalAI LLaMA 7B model

---
+++ nlp/llm/llama-7b/colossalai/README.md
@@ -0,0 +1,64 @@
+# ColossalAI LLaMA-7B
+
+## Model description
+Llama 7B is part of a collection of foundation language models called **Llama**, which
+range from 7B to 65B parameters. Llama models are trained on trillions of tokens from
+publicly available datasets, and achieve state-of-the-art performance on various natural
+language understanding tasks. Llama 7B is the smallest model in the Llama family, but it
+still has impressive capabilities. It can generate fluent and coherent text, answer
+questions, complete sentences, and more.
+
+ColossalChat is the project to implement LLM with RLHF, powered by the Colossal-AI project.
+
+Coati stands for ColossalAI Talking Intelligence. It is the name for the module implemented in this project and is also the name of the large language model developed by the ColossalChat project.
+
+## Step 1: Installation
+
+### Install ColossalAI
+LLaMA-7B model is using Colossalai toolbox. Before you run this model, you need to setup Colossalai first.
+
+```shell
+cd ../../../../toolbox/ColossalAI/
+bash install_toolbox_colossalai.sh
+```
+
+### Install coati
+```shell
+cd ColossalAI/applications/Chat
+pip3 install .
+```
+## Step 2: Preparing datasets
+
+You can download dataset and pretrained mode from below link.
+- instinwild_en.json: [BaiduPan](https://pan.baidu.com/s/1f22_1dcWr-ZwErOo8OwbzQ?pwd=x3s9), [GoogleDrive](https://drive.google.com/file/d/1qOfrl0RIWgH2_b1rYCEVxjHF3u3Dwqay/view)
+- [llama-7b-hf](https://huggingface.co/decapoda-research/llama-7b-hf)
+
+## Step 3: Training
+```shell
+# multi node
+torchrun --nnodes=$NODE_NUMS --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --nproc_per_node=8 --master_port $MASTER_PORT examples/train_sft.py \
+    --pretrain /path/to/llama-7b-hf \
+    --model 'llama' \
+    --strategy colossalai_zero2_cpu \
+    --log_interval 10 \
+    --save_path  ${MODEL_SAVE_PATH} \
+    --dataset /path/to/instinwild_en.json \
+    --batch_size 1 \
+    --accumulation_steps 8 \
+    --lr 2e-5 \
+    --max_datasets_size 512 \
+    --max_epochs 1
+```
+ If the torchrun command cannot be found，you can execute
+```shell
+ln -s /usr/local/corex-3.1.0/lib64/python3/dist-packages/bin/torchrun /usr/local/bin/
+```
+## Results
+| Model       | Train speed     |
+|-------------|-----------------|
+| LLaMA-7B    | 0.9 samples/sec |
+
+## no
+## Reference
+
+- [ColossalAI](https://github.com/hpcaitech/ColossalAI)
---
+++ toolbox/ColossalAI/patch/applications/Chat/coati/trainer/sft.py
@@ -0,0 +1,139 @@
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd. # All Rights Reserved.
+
+import math
+import time
+from typing import List, Optional
+
+import torch
+import torch.distributed as dist
+import wandb
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.trainer import get_scheduler
+
+from .base import Trainer
+from .callbacks import Callback
+from .strategies import ColossalAIStrategy, Strategy
+from .utils import is_rank_0, to_device
+
+
+class SFTTrainer(Trainer):
+    """
+        Trainer to use while training reward model.
+
+    Args:
+        model (torch.nn.Module): the model to train
+        strategy (Strategy): the strategy to use for training
+        optim(Optimizer): the optimizer to use for training
+        train_dataloader: the dataloader to use for training
+        eval_dataloader: the dataloader to use for evaluation
+        batch_size (int, defaults to 1): the batch size while training
+        max_epochs (int, defaults to 2): the number of epochs to train
+        callbacks (List[Callback], defaults to []): the callbacks to call during training process
+        optim_kwargs (dict, defaults to {'lr':1e-4}): the kwargs to use while initializing optimizer
+    """
+
+    def __init__(
+        self,
+        model,
+        strategy: Strategy,
+        optim: Optimizer,
+        train_dataloader: DataLoader,
+        eval_dataloader: DataLoader = None,
+        max_epochs: int = 2,
+        accumulation_steps: int = 8,
+        callbacks: List[Callback] = [],
+    ) -> None:
+        if accumulation_steps > 1 and isinstance(strategy, ColossalAIStrategy) and strategy.stage == 3:
+            raise ValueError("Accumulation steps are not supported in stage 3 of ColossalAI")
+        super().__init__(strategy, max_epochs, callbacks=callbacks)
+        self.train_dataloader = train_dataloader
+        self.eval_dataloader = eval_dataloader
+        self.model = model
+        self.optimizer = optim
+
+        self.accumulation_steps = accumulation_steps
+        num_update_steps_per_epoch = len(train_dataloader) // self.accumulation_steps
+        max_steps = math.ceil(self.max_epochs * num_update_steps_per_epoch)
+
+        self.scheduler = get_scheduler("cosine",
+                                       self.optimizer,
+                                       num_warmup_steps=math.ceil(max_steps * 0.03),
+                                       num_training_steps=max_steps)
+
+    def fit(self, logger, use_wandb: bool = False):
+        if use_wandb:
+            wandb.init(project="Coati", name=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            wandb.watch(self.model)
+        total_loss = 0
+        # epoch_bar = tqdm(range(self.epochs), desc='Epochs', disable=not is_rank_0())
+        step_bar = tqdm(range(len(self.train_dataloader) // self.accumulation_steps * self.max_epochs),
+                        desc=f'steps',
+                        disable=not is_rank_0())
+        for epoch in range(self.max_epochs):
+
+            # process_bar = tqdm(range(len(self.train_dataloader)), desc=f'Train process for{epoch}', disable=not is_rank_0())
+            # train
+            self.model.train()
+            for batch_id, batch in enumerate(self.train_dataloader):
+                batch = to_device(batch, torch.cuda.current_device())
+                outputs = self.model(batch["input_ids"], attention_mask=batch["attention_mask"], labels=batch["labels"])
+
+                loss = outputs.loss
+
+                if loss >= 2.5 and is_rank_0():
+                    logger.warning(f"batch_id:{batch_id}, abnormal loss: {loss}")
+
+                loss = loss / self.accumulation_steps
+
+                self.strategy.backward(loss, self.model, self.optimizer)
+
+                total_loss += loss.item()
+
+                # gradient accumulation
+                if (batch_id + 1) % self.accumulation_steps == 0:
+                    self.strategy.optimizer_step(self.optimizer)
+                    self.optimizer.zero_grad()
+                    self.scheduler.step()
+                    # if is_rank_0() and use_wandb:
+                    #     wandb.log({
+                    #         "loss": total_loss / self.accumulation_steps,
+                    #         "lr": self.scheduler.get_last_lr()[0],
+                    #         "epoch": epoch,
+                    #         "batch_id": batch_id
+                    #     })
+                    if is_rank_0():
+                        print(f"loss:{total_loss / self.accumulation_steps}, lr: {self.scheduler.get_last_lr()[0]}, epoch: {epoch}, batch_id: {batch_id} ")
+
+                    total_loss = 0
+                    step_bar.update()
+
+                # if batch_id % log_interval == 0:
+                # logger.info(f'Train Epoch {epoch}/{self.epochs} Batch {batch_id} Rank {dist.get_rank()} loss {loss.item()}')
+                # wandb.log({"loss": loss.item()})
+
+                # process_bar.update()
+
+            # eval
+            if self.eval_dataloader is not None:
+                self.model.eval()
+                with torch.no_grad():
+                    loss_sum = 0
+                    num_seen = 0
+                    for batch in self.eval_dataloader:
+                        batch = to_device(batch, torch.cuda.current_device())
+                        outputs = self.model(batch["input_ids"],
+                                             attention_mask=batch["attention_mask"],
+                                             labels=batch["labels"])
+                        loss = outputs.loss
+
+                        loss_sum += loss.item()
+                        num_seen += batch["input_ids"].size(0)
+
+                    loss_mean = loss_sum / num_seen
+                    if dist.get_rank() == 0:
+                        logger.info(f'Eval Epoch {epoch}/{self.max_epochs} loss {loss_mean}')
+
+            # epoch_bar.update()
