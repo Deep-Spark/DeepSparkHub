af9901d5264440ec25d77e6c41b4e8c30f7fb72f
gaiqin_bai <gaiqin.bai@iluvatar.com>
2023-09-18 03:39:35 +0000

    !196 Add VGG16 model on TensorFlow * Add VGG16 model on TensorFlow

---
+++ cv/classification/vgg/tensorflow/get_num_devices.sh
@@ -0,0 +1,25 @@
+# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+devices=$CUDA_VISIBLE_DEVICES
+if [ -n "$devices"  ]; then
+    _devices=(${devices//,/ })
+    num_devices=${#_devices[@]}
+else
+    num_devices=8
+    export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+    echo "Not found CUDA_VISIBLE_DEVICES, set nproc_per_node = ${num_devices}"
+fi
+export IX_NUM_CUDA_VISIBLE_DEVICES=${num_devices}
