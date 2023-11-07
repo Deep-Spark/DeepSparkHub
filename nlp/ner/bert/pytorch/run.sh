d198a360042a893464bcbb36728bc88902d45a7f
majorli <mingjiang.li@iluvatar.com>
2023-11-07 17:36:44 +0800

    update BERT NER to run offline
    Signed-off-by: majorli <mingjiang.li@iluvatar.com>

--- nlp/ner/bert/pytorch/run.sh
+++ nlp/ner/bert/pytorch/run.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 python3 run_ner.py \
-  --model_name_or_path bert-base-uncased \
+  --model_name_or_path ./bert-base-uncased \
   --dataset_name conll2003 \
   --output_dir /tmp/test-ner \
   --do_train \
