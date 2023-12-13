c994f6bcdac277bb5cd99fac57a1a8dd7ab15dd1
songjian <ex.jian.song@iluvatar.com>
2023-12-13 10:05:27 +0000

    update cv/detection/yolov3/pytorch/setup.sh.
    Signed-off-by: songjian <ex.jian.song@iluvatar.com>

--- cv/detection/yolov3/pytorch/setup.sh
+++ cv/detection/yolov3/pytorch/setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # Install packages
 echo "Start installing packages..."
-sudo -E pip3 install tqdm
-sudo -E pip3 install terminaltables
+pip3 install tqdm
+pip3 install terminaltables
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 if [[ ${ID} == "ubuntu" ]]; then
   echo ${ID}
-  sudo -E apt -y install libgl1-mesa-glx
-  sudo -E apt -y install libgeos-dev
+apt -y install libgl1-mesa-glx
+apt -y install libgeos-dev
 elif [[ ${ID} == "Loongnix" ]]; then
   echo ${ID}
-  sudo -E apt -y install libgl1-mesa-glx
-  sudo -E apt -y install libgeos-dev
+apt -y install libgl1-mesa-glx
+apt -y install libgeos-dev
 elif [[ ${ID} == "centos" ]]; then
   echo ${ID}
-  sudo -E yum -y install mesa-libGL
-  sudo -E yum -y install geos-devel
+yum -y install mesa-libGL
+yum -y install geos-devel
 elif [[ ${ID} == "kylin" ]]; then
   echo ${ID}
-  sudo -E yum -y install mesa-libGL
-  sudo -E yum -y install geos-devel
+yum -y install mesa-libGL
+yum -y install geos-devel
 else
   echo "Unable to determine OS..."
 fi
-sudo -E pip3 install cython # Will automatically install opencv-python
-sudo -E pip3 install imgaug # Will automatically install opencv-python
-sudo -E pip3 install torchsummary
+pip3 install cython # Will automatically install opencv-python
+pip3 install imgaug # Will automatically install opencv-python
+pip3 install torchsummary
 
 echo "Finished installing packages."
 
