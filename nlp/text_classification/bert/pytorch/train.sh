35eb5d5351afa5aa9d6701f6a9b576223f8ac03d
majorli <mingjiang.li@iluvatar.com>
2023-12-18 15:00:45 +0800

    imporve bert text classification to run offline
    Signed-off-by: majorli <mingjiang.li@iluvatar.com>

--- nlp/text_classification/bert/pytorch/train.sh
+++ nlp/text_classification/bert/pytorch/train.sh
@@ -16,7 +16,7 @@
 export TASK_NAME=WNLI
 
 python3 run_glue.py \
-  --model_name_or_path bert-base-cased \
+  --model_name_or_path ./bert-base-cased \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
