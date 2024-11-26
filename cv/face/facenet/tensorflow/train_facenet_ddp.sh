e9e85239b64f566e4627ac1ced7118b4abb76792
honglyua <hongliang.yuan@iluvatar.com>
2024-11-26 05:29:05 +0000

    update cv/face/facenet/tensorflow/train_facenet_ddp.sh.
    Signed-off-by: honglyua <hongliang.yuan@iluvatar.com>

--- cv/face/facenet/tensorflow/train_facenet_ddp.sh
+++ cv/face/facenet/tensorflow/train_facenet_ddp.sh
@@ -12,7 +12,8 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.EXIT_STATUS=0
+# limitations under the License.
+EXIT_STATUS=0
 check_status()
 {
     if ((${PIPESTATUS[0]} != 0)); then
