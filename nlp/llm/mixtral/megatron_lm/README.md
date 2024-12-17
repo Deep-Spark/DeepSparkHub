4f922d3eedac505acc68538708505cfb21e30d50
mingjiang.li <mingjiang.li@iluvatar.com>
2024-12-17 11:24:04 +0800

    add mixtral megatron-lm model base on pr 423
    Signed-off-by: mingjiang.li <mingjiang.li@iluvatar.com>

---
+++ nlp/llm/mixtral/megatron_lm/README.md
@@ -0,0 +1,39 @@
+# Mixtral （Megatron-LM)
+
+## Model description
+
+The Mixtral model is a Mixture of Experts (MoE)-based large language model developed by Mistral AI, an innovative company focusing on open-source AI models. Mixtral is designed to achieve high performance while maintaining computational efficiency, making it an excellent choice for real-world applications.
+
+## Step 1: Installation
+
+```sh
+# uninstall
+pip3 uninstall -y megatron-lm
+
+# clone and install
+git clone https://github.com/NVIDIA/Megatron-LM.git
+cd Megatron-LM/ && git checkout bd677bfb13ac2f19deaa927adc6da6f9201d66aa
+## apply patch
+cp -r -T ../../../../toolbox/Megatron-LM/patch ./Megatron-LM/
+## install
+cd Megatron-LM/
+python3 setup.py develop
+```
+
+## Step 2: Preparing datasets
+
+```sh
+cd datasets/
+bash download_and_convert_dataset.sh
+```
+
+## Step 3: Training
+
+```bash
+cd examples/mixtral
+bash train_mixtral_8x7b_distributed.sh
+```
+
+## Reference
+
+- [Megatron-LM](https://github.com/NVIDIA/Megatron-LM/tree/main/examples/mixtral)
---
+++ toolbox/Megatron-LM/patch/datasets/download_and_convert_dataset.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Copyright (c) 2024, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -euox pipefail
+
+CUR_DIR=$(pwd)
+if [[ ! -f $CUR_DIR/small-117M.train.jsonl ]]; then
+    wget http://files.deepspark.org.cn:880/deepspark/small-117M.train.jsonl
+fi
+
+if [[ ! -f $CUR_DIR/tokenizer.model ]]; then
+    wget -O tokenizer.model http://files.deepspark.org.cn:880/deepspark/megatron-lm_tokenizer.model
+fi
+
+PROJ_HOME=$(dirname "$PWD")
+SAVE_PATH=./gpt_small_117M_Mixtral
+mkdir -p $SAVE_PATH
+
+TOKENIZER=Llama2Tokenizer
+TOKENIZER_PATH=./tokenizer.model
+
+python3 $PROJ_HOME/tools/preprocess_data.py \
+            --input ./small-117M.train.jsonl \
+            --json-keys text \
+            --tokenizer-type $TOKENIZER \
+            --tokenizer-model $TOKENIZER_PATH \
+            --output-prefix $SAVE_PATH/gpt_small_117M \
+            --append-eod \
+            --workers 32
+
+rm -f small-117M.train.jsonl
