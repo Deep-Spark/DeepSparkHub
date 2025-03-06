efd115e3d5a66d1d30806ffe8b9b94b96f121f0a
hongliang.yuan <hongliang.yuan@iluvatar.com>
2025-03-06 10:10:54 +0800

    update esrgan

--- cv/super_resolution/esrgan/pytorch/mmedit/models/restorers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-# Copyright (c) 2022, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
-# All Rights Reserved.
-
-from .esrgan import ESRGAN
-from .srgan import SRGAN
-from .basic_restorer import BasicRestorer
