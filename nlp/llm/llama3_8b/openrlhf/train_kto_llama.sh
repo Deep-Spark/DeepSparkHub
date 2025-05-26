146ab4b1c2e8f8e0fe1bb20046e896af176d15cb
hongliang.yuan <hongliang.yuan@iluvatar.com>
2025-05-26 17:38:56 +0800

    add Llama3-8B DPO KTO PPO use openrlhf

---
+++ nlp/llm/llama3_8b/openrlhf/train_kto_llama.sh
@@ -0,0 +1,31 @@
+set -x
+
+read -r -d '' training_commands <<EOF
+openrlhf.cli.train_kto \
+   --save_path ./checkpoint/llama3-8b-kto \
+   --save_steps -1 \
+   --logging_steps 1 \
+   --eval_steps -1 \
+   --train_batch_size 32 \
+   --micro_train_batch_size 1 \
+   --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
+   --bf16 \
+   --max_epochs 1 \
+   --max_len 1024 \
+   --zero_stage 3 \
+   --learning_rate 5e-7 \
+   --dataset OpenRLHF/ultrafeedback-unpaired-preferences \
+   --input_key instruction \
+   --output_key response \
+   --label_key score \
+   --flash_attn \
+   --beta 0.1 \
+   --max_samples 1024 \
+   --gradient_checkpointing
+EOF
+    # --use_wandb [WANDB_TOKENS] or True (use wandb login command)
+
+
+if [[ ${1} != "slurm" ]]; then
+    deepspeed --module $training_commands
+fi
