e5d4b1b142bed4b0487f5a6b1ee2d429723620d0
hongliang.yuan <hongliang.yuan@iluvatar.com>
2025-09-10 17:37:00 +0800

    fix license

--- nlp/text_classification/bert/pytorch/train_dist_torch2.sh
+++ nlp/text_classification/bert/pytorch/train_dist_torch2.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# Copyright (c) 2025, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
