8434d476d2ae46d9a549f3776b3042a7d3ad0778
hongliang.yuan <hongliang.yuan@iluvatar.com>
2025-09-26 11:00:11 +0800

    sync mobilenetv3

---
+++ tests/executables/mobilenetv3/init_torch.sh
@@ -0,0 +1,15 @@
+bash ../_utils/init_classification_torch.sh ../_utils
+
+# determine whether the user is root mode to execute this script
+prefix_sudo=""
+current_user=$(whoami)
+if [ "$current_user" != "root" ]; then
+    echo "User $current_user need to add sudo permission keywords"
+    prefix_sudo="sudo"
+fi
+
+echo "prefix_sudo= $prefix_sudo"
+
+command -v yum >/dev/null && $prefix_sudo yum install -y numactl ||  $prefix_sudo apt install -y numactl
+
+pip3 install -r ../../cv/classification/mobilenetv3/pytorch/requirements.txt
---
+++ tests/executables/mobilenetv3/train_mobilenetv3_large_amp_torch.sh
@@ -0,0 +1,25 @@
+
+CURRENT_DIR=$(cd `dirname $0`; pwd)
+source ../_utils/get_num_devices.sh
+source ../_utils/global_environment_variables.sh
+
+: ${BATCH_SIZE:=32}
+
+ROOT_DIR=${CURRENT_DIR}/../..
+DATA_DIR=${ROOT_DIR}/data/datasets/imagenette
+
+EXIT_STATUS=0
+check_status()
+{
+    if ((${PIPESTATUS[0]} != 0)); then
+        EXIT_STATUS=1
+    fi
+}
+
+cd $CURRENT_DIR/../../cv/classification/mobilenetv3/pytorch/
+ixdltest-check --nonstrict_mode_args="--epoch ${NONSTRICT_EPOCH}" -b 10 --run_script \
+python3 train.py --model  mobilenet_v3_large --data-path "${DATA_DIR}" \
+   --epochs 600 --batch-size ${BATCH_SIZE} --opt sgd --lr 0.1 \
+    --wd 0.00001 --lr-step-size 2 --lr-gamma 0.973 --auto-augment imagenet --random-erase 0.2 \
+	--output-dir . --amp "$@"; check_status
+exit ${EXIT_STATUS}
